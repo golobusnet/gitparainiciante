f3dc64f82f73e69b23128713b4b5edd160428f13
Paulo Ferreira <paulorsf.30@gmail.com>
2024-01-16 19:39:50 -0300

    nova rota adcionada
    teste

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.status(200).send({ message: 'Olá mundo esse é meu novo repositorio clonado.'})
 })
 
+app.get('/ola', (req, res) =>{
+    res.status(200).send({ message: 'olá'});
+});
+
 app.listen(4001, ()=>{
     console.log('Api inicializada na porta 4001');
 })
